e35998f3b628fd912915de3bc6ee502ecea2bc40
MattHughson! <matt@koodomasson.com>
2019-06-05 00:29:40 +0000

    starting activity two

--- express_server.js
+++ express_server.js
@@ -4,10 +4,24 @@ var PORT = 8080; // default port 8080
 
 app.set("view engine", "ejs"); //Installing and Setting Up EJS
 
+
 var urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
+
+function generateRandomString() {
+
+}
+
+
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.get("/urls/new", (req, res) => {
+    res.render("urls_new");
+  });
+
 app.get("/hello", (req, res) => {
     let templateVars = { greeting: 'Hello World!' };
     res.render("hello_world", templateVars);
@@ -41,5 +55,8 @@ app.get("/urls.json", (req, res) => {
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
-
+app.post("/urls", (req, res) => {
+    console.log(req.body);  // Log the POST request body to the console
+    res.send("Ok");         // Respond with 'Ok' (we will replace this)
+  });
 
